cdbf92c21da52a73d836d40d73af7fed3ba26949
vessbon <115010176+vessbon@users.noreply.github.com>
2025-12-12 22:25:11 +0100

    chore: add logger middleware and use type safe .env

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
+import env from "./env";
 import indexRouter from "./routes/index.route";
 import urlRouter from "./routes/urls.routes";
 
 const app = new Hono();
+app.use(logger());
 
 app.route("/", indexRouter);
 app.route("/urls", urlRouter);
 
-export default app;
+export default {
+  PORT: env.PORT,
+  fetch: app.fetch,
+};
