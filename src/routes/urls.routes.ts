9e120d3b9bf2b18f55fe9d0d4ebfb1257f9ba30a
vessbon <115010176+vessbon@users.noreply.github.com>
2025-12-13 15:20:45 +0100

    feat: create list urls route

--- src/routes/urls.routes.ts
+++ src/routes/urls.routes.ts
@@ -1,5 +1,23 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import db from "@/db";
+import { urlInsertSchema, urls } from "@/db/schema";
+import { generateCode } from "@/utils/generate-code";
 
 const urlRouter = new Hono();
+
+urlRouter.get("/", async (c) => {
+  const result = await db.select().from(urls);
+  return c.json(result);
+});
+
+urlRouter.post("/", zValidator("json", urlInsertSchema), async (c) => {
+  const { url } = c.req.valid("json");
+  const code = generateCode();
+
+  const [inserted] = await db.insert(urls).values({ url, code }).returning();
+
+  return c.json(inserted);
+});
 
 export default urlRouter;
